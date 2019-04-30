7a1dba553b2f1db675f4bea474dbd3eca93d1103
watsonLidaye <610466535@qq.com>
2019-04-30 11:24:02 +0800

    更新做前端修改

--- page/tabBar/person/expire.js
+++ page/tabBar/person/expire.js
@@ -1,27 +1,17 @@
-// page/tabBar/index/detail/index.js
+// page/tabBar/person/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    header_type:'time',
-    select_bottom:'1'
+    infoShow:true, //头部信息是否显示
+    isEnter:false  //是否已经入住
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  headerOp(e){
-    const type = e.currentTarget.dataset.type
-    if(type !=this.data.header_type) {
-      this.setData({
-        header_type:type,
-        select_bottom:'1'
-      })
-    }
-    
-  },
   onLoad: function (options) {
 
   },
@@ -74,17 +64,10 @@ Page({
   onShareAppMessage: function () {
 
   },
-  navigateTo(){
-    wx.reLaunch({
-      url: '/page/tabBar/index/detail/result',
-    });
-  },
-  switchact(e){
-    const index = e.currentTarget.dataset.index
-    if(index!=this.data.select_bottom){
-      this.setData({
-        select_bottom:index
-      })
-    }
+  closeInfo(){
+  
+    this.setData({
+      infoShow:false
+    })
   }
 })
